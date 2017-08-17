2948c7a66ad00cb0e03b9baf77c976552939368e
Divya-Yerramsetty <divya15y@gmail.com>
2017-08-17 16:05:46 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,11 @@
-console.log('Loaded!');
-
-var element = document.getElementById ('main-text');
-element.innerHTML = "This is a new app";
-
-var img = document.getElementById ('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft  = marginLeft + 'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight, 100);
+//Counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function() {
+    
+    
+    //Render the variable in the correct span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
