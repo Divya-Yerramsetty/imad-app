2ab85d6fa298675a3e0c2cfbce32b1aec3e56d56
Divya-Yerramsetty <divya15y@gmail.com>
2017-08-16 19:06:00 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element = document.getElementById ('main-text');
 element.innerHTML = "This is a new app";
 
-var img = document.getElementById ('img');
+var img = document.getElementById ('madi');
 img.onclick = function() {
     img.style.marginleft = '100px';
 };
