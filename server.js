dfbefb968c402d7391a54ee67055013759b10449
Divya-Yerramsetty <divya15y@gmail.com>
2017-08-31 21:37:50 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,6 @@ app.get('submit-comment/:comment', function(req,res) {
 
 app.get('/articles/:articleName', function (req,res) {
 
-    
     pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result) {
         if (err) {
             res.status(500).send(err.toString());
